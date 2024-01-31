83d53418746bf1ba1b5e7d92c2452cdaa3146e69
Jonathan Clerence <johanclerence@gmail.com>
2024-01-31 19:52:06 +0530

    Added new Blazor app and new components

---
+++ CleanArchWithCQRSandMediator.Blazor/Program.cs
@@ -0,0 +1,35 @@
+using CleanArchWithCQRSandMediator.Application;
+using CleanArchWithCQRSandMediator.Blazor.Data;
+using CleanArchWithCQRSandMediator.Infra;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
